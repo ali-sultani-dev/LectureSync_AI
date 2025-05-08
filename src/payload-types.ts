59d1d48189331e12857c879b741ff3c82c231ba8
ali-sultani-dev <ali.sultani.dev@gmail.com>
2025-05-09 02:08:26 +1200

    add Notes to config

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -140,6 +140,7 @@ export interface User {
 export interface Media {
   id: number;
   alt: string;
+  owner?: (number | null) | User;
   updatedAt: string;
   createdAt: string;
   url?: string | null;
@@ -233,6 +234,7 @@ export interface UsersSelect<T extends boolean = true> {
  */
 export interface MediaSelect<T extends boolean = true> {
   alt?: T;
+  owner?: T;
   updatedAt?: T;
   createdAt?: T;
   url?: T;
