4bc844b62fe06058092c1c4cce4715c4bce724d5
ali-sultani-dev <ali.sultani.dev@gmail.com>
2025-05-09 11:49:55 +1200

    add route logged out users to login page

---
+++ src/components/SignedIn.jsx
@@ -0,0 +1,18 @@
+import { headers as getHeaders } from 'next/headers'
+import { redirect } from 'next/navigation'
+import { getPayload } from 'payload'
+import config from '@/payload.config'
+import React from 'react'
+
+export default async function SignedIn({ children }) {
+  const headers = await getHeaders()
+  const payloadConfig = await config
+  const payload = await getPayload({ config: payloadConfig })
+  const { user } = await payload.auth({ headers })
+
+  if (!user) {
+    redirect('/login')
+  }
+
+  return <>{children}</>
+}
